a8232b75b70643d93f6e22d47bb65c6cafbe32d8
Deimos308 <andrew.saienko.308@gmail.com>
2024-03-08 00:58:56 +0200

    🩹 (global): minor fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
 require("dotenv").config();
 import express from "express";
-import passport from "passport";
+// import passport from "passport";
 import cookieSession from "cookie-session";
 import cookieParser from "cookie-parser";
 
 import projectRouter from "./routes/project";
 import authRouter from "./routes/auth";
 
-const passportSetup = require("./config/passportSetup");
+// const passportSetup = require("./config/passportSetup");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,13 +16,13 @@ app.use(
   cookieSession({
     name: "session",
     keys: [process.env.COOKIE_KEY],
-    maxAge: 24 * 60 * 60 * 100
+    maxAge: 24 * 60 * 60 * 100,
   })
 );
 
 app.use(express.json());
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(cookieParser());
 
 app.use("/auth", authRouter);
@@ -32,7 +32,7 @@ const authCheck = (req, res, next) => {
   if (!req.user) {
     res.status(401).json({
       authenticated: false,
-      message: "user has not been authenticated"
+      message: "user has not been authenticated",
     });
   } else {
     next();
@@ -44,7 +44,7 @@ app.get("/", authCheck, (req, res) => {
     authenticated: true,
     message: "user successfully authenticated",
     user: req.user,
-    cookies: req.cookies
+    cookies: req.cookies,
   });
 });
 
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import passport from "passport";
+// import passport from "passport";
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ import * as controller from "../controllers/auth";
 
 router.get("/login/success", controller.loginSuccess);
 router.get("/login/failed", controller.loginFail);
-router.get("/github", passport.authenticate("github", { scope: [ 'user:email' ] }));
-router.get("/github/callback", passport.authenticate('github', { failureRedirect: '/login', successRedirect: process.env.CLIENT_URL }));
+// router.get("/github", passport.authenticate("github", { scope: [ 'user:email' ] }));
+// router.get("/github/callback", passport.authenticate('github', { failureRedirect: '/login', successRedirect: process.env.CLIENT_URL }));
 
 export default router;
